d987bb08a62cf84eb03240e19daf7b2d27f61506
boris37544@gmail.com <mygmail@gmail.com>
2021-11-22 17:31:04 +0300

    add login system

--- AutoRep/Models/User.cs
+++ AutoRep/Models/User.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace AutoRep.Models
+﻿namespace AutoRep.Models
 {
     public class User
     {
-        public User() { }
+        public User()
+        { }
+
         public int Id { get; set; }
         public string Name { get; set; }//ФИО работника/гово
         public bool IsOwner { get; set; }//t = владелец, f = работник
+
         public override string ToString()
         {
             return Id.ToString() + " " + Name + " " + IsOwner.ToString();
         }
+
         public enum SortState
         {
             NameAsc,
