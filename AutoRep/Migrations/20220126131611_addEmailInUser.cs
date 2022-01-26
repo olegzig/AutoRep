2df97e6d851caadab4cdc3a47d33dd6231f26341
olegzig <boris37544@gmail.com>
2022-01-26 16:16:48 +0300

    add migration

---
+++ AutoRep/Migrations/20220126131611_addEmailInUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AutoRep.Migrations
+{
+    public partial class addEmailInUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Request",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Request");
+        }
+    }
+}
