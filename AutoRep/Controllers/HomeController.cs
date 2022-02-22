f3f94bb943b9353d80dd84578a3ea02d5feb74ac
olegzig <boris37544@gmail.com>
2022-02-23 00:01:56 +0300

    translate index. get same error into request

--- AutoRep/Controllers/HomeController.cs
+++ AutoRep/Controllers/HomeController.cs
@@ -1,29 +1,106 @@
-﻿using AutoRep.Models;
+﻿using AutoRep.Data;
+using AutoRep.Models;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AutoRep.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration Configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration config, ApplicationDbContext context)
         {
+            _context = context;
             _logger = logger;
+            Configuration = config;
         }
 
         public IActionResult Index()
         {
+            GetWorkTypeList();
             return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(UserRequest request)
+        {
+            if (ModelState.IsValid)
+            {
+                request.WorkType = string.Join(",", request.WorkTypeIds);
+                _context.Add(request);
+                await _context.SaveChangesAsync();
+                if (!User.Identity.IsAuthenticated)
+                {
+                    return View("../Home/Index");
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(request);
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+        // make a viewbug of workTypes
+        public List<WorkType> GetWorkTypeList()
+        {
+            var connection = Configuration.GetConnectionString("DefaultConnection");
+            SqlConnection con = new SqlConnection(connection);
+            SqlCommand cmd = new SqlCommand("select [id],[name] from [WorkType]", con);
+            con.Open();
+            SqlDataReader idr = cmd.ExecuteReader();
+
+            List<WorkType> workTypes = new List<WorkType>();
+            if (idr.HasRows)
+            {
+                while (idr.Read())
+                {
+                    workTypes.Add(new WorkType { Id = Convert.ToInt32(idr["Id"]), Name = Convert.ToString(idr["Name"]) });
+                }
+            }
+
+            con.Close();
+            ViewBag.WorkTypeBag = workTypes;
+            return workTypes;
+        }
+
+        //make a string of worktypes that used in this work
+        public string GetWorkTypeListString(string[] WTIds)
+        {
+            var connection = Configuration.GetConnectionString("DefaultConnection");
+            SqlConnection con = new SqlConnection(connection);
+            SqlCommand cmd = new SqlCommand("select [id],[name] from [WorkType]", con);
+            con.Open();
+            SqlDataReader idr = cmd.ExecuteReader();
+
+            List<WorkType> workTypes = new List<WorkType>();
+            if (idr.HasRows)
+            {
+                while (idr.Read())
+                {
+                    if (WTIds.Contains(idr["id"].ToString()))
+                        workTypes.Add(new WorkType { Id = Convert.ToInt32(idr["Id"]), Name = Convert.ToString(idr["Name"]) });
+                }
+            }
+
+            con.Close();
+            string x = String.Join(", ", workTypes.Select(x => x.Name));
+            return x;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
